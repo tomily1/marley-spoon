151ffdd1bc90ff33416fbc568ccdfc0d6d2a689c
Israel Tomilayo <itomilayo@gmail.com>
2021-01-06 16:46:43 +0100

    update contentful api client
    - move marleyspoon cdn client from model concerns to model

--- app/models/concerns/marley_spoon_api.rb
+++
@@ -1,9 +0,0 @@
-class MarleySpoonApi
-  def self.client
-    @client
-  end
-
-  def self.configure(&block)
-    @client ||= yield
-  end
-end
--- config/initializers/contentful.rb
+++ config/initializers/contentful.rb
@@ -2,6 +2,6 @@ MarleySpoonApi.configure do
   Contentful::Client.new(
     space: ENV['SPACE_ID'],
     environment: ENV['ENVIRONMENT'],
-    access_token: ENV['ACCESS_TOKEN']
+    access_token: ENV['ACCESS_TOKEN'],
   )
 end
