4f66a5b39f21038f81f0fa55675d663f458693ff
Israel Tomilayo <itomilayo@gmail.com>
2021-01-07 13:20:17 +0100

    show specific menu on the show page

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require "action_cable/engine"
 require 'pry'
 
 # Require the gems listed in Gemfile, including any gems
