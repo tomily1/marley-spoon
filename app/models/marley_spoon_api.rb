629fdf575525e6c45f9145915dff257d83011f1a
Israel Tomilayo <itomilayo@gmail.com>
2021-01-07 17:43:24 +0100

    remove unused code from api client

--- app/models/marley_spoon_api.rb
+++ app/models/marley_spoon_api.rb
@@ -5,18 +5,6 @@ class MarleySpoonApi
     @client ||= contentful_client
   end
 
-  # def self.with_caching(key, expires_in = 1.day.from_now, &block)
-  #   if cache[key].nil? || cache[key][:expiration_time] < Time.now.to_i
-  #     cache[key] = { 'value' => yield(block), 'expiration_time' => expires_in }
-  #   end
-
-  #   cache[key][:value]
-  # end
-
-  # def self.cache
-  #   @cache ||= HashWithIndifferentAccess.new
-  # end
-
   def self.contentful_client
     Contentful::Client.new(
       space: ENV['SPACE_ID'],
